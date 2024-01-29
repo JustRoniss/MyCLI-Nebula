aba2ad8455760e35edf88292289e8b800206b099
JustRoniss <ronaldoluizdev@gmail.com>
2024-01-29 11:03:25 -0300

    Alguns ajustes e inclusao jmidas

--- main.py
+++ main.py
@@ -1,49 +1,51 @@
 from menu import menu_principal
-from commands import buildGateways, deployGateways
-
-import os
+from commands import buildGateways, deployGateways, buildJmidas, deployJmidas
 import argparse
 
-def main():
-    
+def parse_args():
     parser = argparse.ArgumentParser(description="Automatizador de comandos do nebula")
-
     parser.add_argument("--build", help="informe o serviço que irá receber o build")
     parser.add_argument("--tag", help="informe a tag para ser buildada")
     parser.add_argument("--deploy", help="informe o serviço que irá receber o deploy")
     parser.add_argument("--env", help="informe o environment onde será realizado o deploy")
+    return parser.parse_args()
 
-    args = parser.parse_args()
-
-
-    if args.build and not args.tag:
-        parser.error("--tag é obrigatório quando --build é fornecido")
-
-    elif args.deploy and not args.tag:
-        parser.error("--tag é obrigatório quando --deploy é fornecido")
-
-    elif args.deploy and not args.env:
+def validate_args(args, parser):
+    if (args.build or args.deploy) and not args.tag:
+        parser.error("--tag é obrigatório quando --build ou --deploy são fornecidos")
+    if args.deploy and not args.env:
         parser.error("--env é obrigatório quando --deploy é fornecido")
 
+def execute_command(args):
+    commands = {
+        "cfa-ra-gateways": {
+            "build": buildGateways,
+            "deploy": deployGateways
+        },
+        "cfa-jmidas": {
+            "build": buildJmidas,
+            "deploy": deployJmidas
+        }
+    }
 
+    if args.build:
+        command = commands.get(args.build, {}).get("build")
+        if command:
+            command(args.tag)
 
-    elif (args.build or args.deploy) and args.tag:
-        if args.build:
-            if(args.build == "cfa-ra-gateways"):
-                buildGateways(args.tag)
+    if args.deploy:
+        command = commands.get(args.deploy, {}).get("deploy")
+        if command:
+            command(args.tag, args.env)
 
-        if args.deploy:
-            if(args.deploy == "cfa-ra-gateways"):
-                deployGateways(args.tag, args.env)
-        
+def main():
+    args = parse_args()
+    validate_args(args, argparse.ArgumentParser())
+    
+    if args.build or args.deploy:
+        execute_command(args)
     else:
         menu_principal()
-    
 
 if __name__ == "__main__":
     main()
-
-    # while True:
-    #     menu_principal()
-
-
